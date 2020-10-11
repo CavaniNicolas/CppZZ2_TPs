9e4a2be6e5c79dd9411043eafad7b4bcb6c4d964
Cavani Nicolas <cavani.nicolas6@gmail.com>
2020-10-11 23:36:35 +0200

    makefile now creates 2 exe

---
+++ TP5/build/src/tests/main_test.d
@@ -0,0 +1,1 @@
+build/src/tests/main_test.o: src/tests/main_test.cpp src/tests/catch.hpp
---
+++ TP5/build/src/tests/tests_catch.d
@@ -0,0 +1,2 @@
+build/src/tests/tests_catch.o: src/tests/tests_catch.cpp \
+ src/tests/catch.hpp src/tests/../Chaine.hpp
